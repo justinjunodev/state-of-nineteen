c2adc2088e7abf527176c5017937a1725f31ff97
Justin Juno <hello@justinjuno.dev>
2020-11-21 16:25:26 -0600

    add react-query and useCovidData hook

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import 'reset-css'
+import { QueryCache, ReactQueryCacheProvider } from 'react-query'
 
-const Layout = ({ children }) => <>{children}</>
+const queryCache = new QueryCache()
+
+const Layout = ({ children }) => (
+  <ReactQueryCacheProvider queryCache={queryCache}>
+    {children}
+  </ReactQueryCacheProvider>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
+import useCovidData from '../hooks/useCovidData'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>State of Nineteen</h1>
-  </Layout>
-)
+const IndexPage = () => {
+  const { status, data, error } = useCovidData()
 
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>State of Nineteen</h1>
+      {status === 'loading' && 'loading ...'}
+      {status === 'error' && `error: ${error.message}`}
+      {data && (
+        <ul>
+          {data.map(d => (
+            <li key={d.state}>{d.state}</li>
+          ))}
+        </ul>
+      )}
+    </Layout>
+  )
+}
 export default IndexPage
