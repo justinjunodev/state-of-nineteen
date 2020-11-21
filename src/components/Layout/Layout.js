8bf50be13a6ac7407524c19db7198972be931515
Justin Juno <hello@justinjuno.dev>
2020-11-21 14:30:08 -0600

    config for development

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import 'reset-css'
+
+const Layout = ({ children }) => <>{children}</>
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,11 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from 'react'
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: Page Not Found</h1>
   </Layout>
 )
 
