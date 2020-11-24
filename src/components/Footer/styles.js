a6a43d5390031e501e3247c4192107f02de76c9a
Justin Juno <hello@justinjuno.dev>
2020-11-23 21:23:16 -0600

    add footer wrapper and update spacing

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,15 +1,18 @@
 import styled from 'styled-components'
+import { contentWrapper } from '../../styles/global'
 
 export const FooterContent = styled.footer`
+  ${contentWrapper};
   text-align: center;
+  padding-bottom: var(--scale-6);
 `
 
 export const FooterBuild = styled.p`
-  margin-bottom: var(--scale-3);
+  margin-bottom: var(--scale-1);
 `
 
 export const FooterCredit = styled.p`
-  margin-bottom: var(--scale-6);
+  margin-bottom: var(--scale-4);
 `
 
 export const FooterCopyright = styled.small``
